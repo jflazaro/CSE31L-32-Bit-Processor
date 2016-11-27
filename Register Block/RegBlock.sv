abec6a0fee6371aa8abfe95c606c040e28727e7f
Joshua Lazaro <Joshua Lazaro>
2016-11-26 22:45:10 -0800

    Added ALU to RegBlock to solve rd2 output to 2 ports

--- Register Block/RegBlock.sv
+++ Register Block/RegBlock.sv
@@ -20,7 +20,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module RegBlock(rs, rt, rd, wd, we, clk, mux_sel, imm_in, opA, opB, opBwd);
+module RegBlock(rs, rt, rd, wd, we, clk, mux_sel, imm_in, opA, opB, opBwd, ALUresult, ALUopsel);
 
     parameter RWIDTH = 6;
     parameter DWIDTH = 32;
@@ -34,12 +34,14 @@ module RegBlock(rs, rt, rd, wd, we, clk, mux_sel, imm_in, opA, opB, opBwd);
     input logic                 clk;
     input logic                 mux_sel; //Need to be defined in the controller
     input logic [IMM_IN -1:0]   imm_in;
-    output logic [DWIDTH -1:0]  opA;
-    output logic [DWIDTH -1:0]  opB;
+    input logic [DWIDTH -1:0]  opA;
+    input logic [DWIDTH -1:0]  opB;
+    input logic [3:0] ALUopsel; //[3]:mode [2:0]:opsel
     output logic [DWIDTH -1:0]  opBwd;    
+    output logic [DWIDTH -1:0] ALUresult;
     
     wire [DWIDTH -1:0] IMM_to_MUX;
-    wire [DWIDTH -1:0] RD2_to_MUX;
+    //wire [DWIDTH -1:0] RD2_to_MUX;
 
     regfile_32bit RF(
         .ra1(rs),
@@ -49,8 +51,7 @@ module RegBlock(rs, rt, rd, wd, we, clk, mux_sel, imm_in, opA, opB, opBwd);
         .we(we),
         .clk(clk),
         .rd1(opA),
-        .rd2(RD2_to_MUX)
-        //.rd2(opBwd) // Can you hook to the same wire twice?
+        .rd2(opBwd)
     );
     
     sext SE(
@@ -59,9 +60,21 @@ module RegBlock(rs, rt, rd, wd, we, clk, mux_sel, imm_in, opA, opB, opBwd);
     );
     
     mux2to1 MUX(
-        .A(RD2_to_MUX),
+        .A(opBwd),
         .B(IMM_to_MUX),
         .sel(mux_sel),
         .Y(opB)
+    );
+    
+    alu_32bit ALU(
+        .op1(opA),
+        .op2(opB),
+        .opsel(ALUopsel[2:0]),
+        .mode(ALUopsel[3]),
+        .result(ALUresult),
+        .c_flag(),
+        .z_flag(),
+        .o_flag(),
+        .s_flag()
     );
 endmodule
