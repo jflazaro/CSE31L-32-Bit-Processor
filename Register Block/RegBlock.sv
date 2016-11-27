804a89cfc7f3b7253eff1f3e3228063d535b8e13
vince <vincezhu15@gmail.com>
2016-11-26 17:04:22 -0800

    Combined regfile sext and mux into RegBlock, not tested/final

--- Register Block/RegBlock.sv
+++ Register Block/RegBlock.sv
@@ -20,14 +20,48 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module RegBlock
-( 
-    logic A,
-    logic B,
-    logic sel,
-    output Y
+module RegBlock(rs, rt, rd, wd, we, clk, mux_sel, imm_in, opA, opB, opBwd);
+
+    parameter RWIDTH = 6;
+    parameter DWIDTH = 32;
+    parameter IMM_IN = 15;
+    
+    input logic [RWIDTH -1:0]   rs;
+    input logic [RWIDTH -1:0]   rt;
+    input logic [RWIDTH -1:0]   rd;
+    input logic [DWIDTH -1:0]   wd;
+    input logic                 we;
+    input logic                 clk;
+    input logic                 mux_sel; //Need to be defined in the controller
+    input logic [IMM_IN -1:0]   imm_in;
+    output logic [DWIDTH -1:0]  opA;
+    output logic [DWIDTH -1:0]  opB;
+    output logic [DWIDTH -1:0]  opBwd;    
+    
+    wire [DWIDTH -1:0] IMM_to_MUX;
+    wire [DWIDTH -1:0] RD2_to_MUX;
+
+    regfile_32bit RF(
+        .ra1(rs),
+        .ra2(rt),
+        .wa(rd),
+        .wd(wd),
+        .we(we),
+        .clk(clk),
+        .rd1(opA),
+        .rd2(RD2_to_MUX)
+        //.rd2(opBwd) // Can you hook to the same wire twice?
     );
-
-    assign Y = (sel == 1'b0)? A : B;
-
+    
+    sext SE(
+        .in(imm_in),
+        .out(IMM_to_MUX)
+    );
+    
+    mux2to1 MUX(
+        .A(RD2_to_MUX),
+        .B(IMM_to_MUX),
+        .sel(mux_sel),
+        .Y(opB)
+    );
 endmodule
--- Register Block/RegBlock_tb.sv
+++ Register Block/RegBlock_tb.sv
@@ -19,49 +19,111 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+parameter RWIDTH = 6;
+parameter DWIDTH = 32;
+parameter IMM_IN = 15;
 
 module RegBlock_tb; // Testbenches don't need any ports.
-
-    logic a;
-    logic b;
-    logic sel;
-    wire y;
+    logic [RWIDTH -1:0]   rs;
+    logic [RWIDTH -1:0]   rt;
+    logic [RWIDTH -1:0]   rd;
+    logic [DWIDTH -1:0]   wd;
+    logic                 we;
+    logic                 clk = 0;
+    logic                 mux_sel; //Need to be defined in the controller
+    logic [IMM_IN -1:0]   imm_in;
+    logic [DWIDTH -1:0]  opA;
+    logic [DWIDTH -1:0]  opB;
+    logic [DWIDTH -1:0]  opBwd;    
             
-    RegBlock M1(
-        .A(a),
-        .B(b),
-        .sel(sel),
-        .Y(y)
-        );
+    RegBlock RB1(
+        .rs(rs),
+        .rt(rt),
+        .rd(rd),
+        .wd(wd),
+        .we(we),
+        .clk(clk),
+        .mux_sel(mux_sel),
+        .imm_in(imm_in),
+        .opA(opA),
+        .opB(opB),
+        .opBwd(opBwd)
+    );
        
     initial begin
-        a = 1'b0;
-        b = 1'b0;
-        sel = 1'b0;
-        #10;
-        sel = 1'b1;
-        #10;
-
-        a = 1'b0;
-        b = 1'b1;
-        sel = 1'b0;
-        #10;
-        sel = 1'b1;
-        #10;
-        
-        a = 1'b1;
-        b = 1'b0;
-        sel = 1'b0;
-        #10;
-        sel = 1'b1;
-        #10;
-
-        a = 1'b1;
-        b = 1'b1;
-        sel = 1'b0;
-        #10;
-        sel = 1'b1;
-        #10;
+    
+    imm_in = 15'b001111111111111;
+    mux_sel = 1'b0;         
+      
+    we = 1'b0;
+    rd = 6'b000000;
+    wd = 32'h000000;
+    rs = 6'b000000;
+    rt = 6'b101001;
+    mux_sel = 1;
+    #50;
+    mux_sel = 0;
+    
+    #50;
+    rs = 6'b100011;
+    mux_sel = 1;
+    #50;
+    mux_sel = 0;
+    #50;
+    
+    we = 1'b1;
+    rd = 6'b111111;
+    wd = 32'hFFAAFFAA;
+    rs = 6'b000000;
+    rt = 6'b000000;
+    
+    mux_sel = 1;
+    #50;
+    mux_sel = 0;
+    
+    #50;
+    
+    we = 1'b1;
+    rd = 6'b001100;
+    wd = 32'hAAAAAAAA;
+    rs = 6'b001011;
+    rt = 6'b111111;
+    
+    mux_sel = 1;
+    #50;
+    mux_sel = 0;
+    
+    #50;        
+    we = 1'b0;
+    rs = 6'b001100;
+    rt= 6'b111111;
+    rd = 6'b000000;
+    wd = 32'h00000000;
+    
+    #50;
+    mux_sel = 1;
+    
+//    #50;
+//    ra1 = 6'b000000;
+//    ra2 = 6'b000111;
+    
+//    #50;
+//    we = 1'b1;
+//    wa = 6'b000000;
+//    wd = 32'hBBBBBBBB;
+//    ra2 = 6'b111111;
+    
+//    #50;
+//    we = 0'b0;
+//    ra1 = 6'b000000;
+    
+//    #4;
+//    ra1 = 6'b111111;
+//    #3;
+//    ra2 = 6'b001100;
+    
+//    #50;
 end
            
+always #10 clk = ~clk;
 endmodule
