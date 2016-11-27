24a85964a6ed412d12004e3c90a1fdbfbfb7ac58
ckevinbm <ckbravo@uci.edu>
2016-11-26 17:53:58 -0800

    Add files via upload

---
+++ new/MUX2to1Sele.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/26/2016 04:20:47 PM
+// Design Name: 
+// Module Name: MUX2to1Sele
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module MUX2to1Sele(
+
+    );
+endmodule
