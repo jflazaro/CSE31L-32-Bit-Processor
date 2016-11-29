38322fd3ef8ecfc8cccbdc1042558f394d6ab49b
Joshua Lazaro <Joshua Lazaro>
2016-11-29 15:34:28 -0800

    Version that was turned in

--- FINAL SUBMISSION/TestBenches/mux2to1_tb.sv
+++ FINAL SUBMISSION/TestBenches/mux2to1_tb.sv
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 10/18/2016 11:46:26 AM
+// Create Date: 11/28/2016 08:06:39 PM
 // Design Name: 
-// Module Name: mux2to1_tb
+// Module Name: MUX2to1_TB
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,48 +20,36 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module mux2to1_tb; // Testbenches don't need any ports.
-
-    logic a;
-    logic b;
-    logic sel;
-    wire y;
-            
-    mux2to1 M1(
-        .A(a),
-        .B(b),
-        .sel(sel),
-        .Y(y)
-        );
-       
-    initial begin
-        a = 1'b0;
-        b = 1'b0;
-        sel = 1'b0;
-        #10;
-        sel = 1'b1;
-        #10;
-
-        a = 1'b0;
-        b = 1'b1;
-        sel = 1'b0;
-        #10;
-        sel = 1'b1;
-        #10;
-        
-        a = 1'b1;
-        b = 1'b0;
-        sel = 1'b0;
-        #10;
-        sel = 1'b1;
-        #10;
-
-        a = 1'b1;
-        b = 1'b1;
-        sel = 1'b0;
-        #10;
-        sel = 1'b1;
-        #10;
-end
+module MUX2to1_TB;
+   
+   parameter AWIDTH = 32;
+   
+   logic [AWIDTH -1: 0] Addr;
+   logic [AWIDTH -1: 0] ReaDat;
+   logic MS2;
+   logic [AWIDTH -1: 0] WD;
            
+   MUX2to1Sele MM(
+       .Addr(Addr),
+       .ReaDat(ReaDat),
+       .MS2(MS2),
+       .WD(WD)
+       );
+      
+   initial begin
+       Addr= 32'b11111111111111111111111111111111;
+       ReaDat = 32'b00000000000000000000000000000000;
+       MS2 = 1'b0;
+       #10;
+       MS2 = 1'b1;
+       #10;
+
+       Addr = 32'b101010101010101010101010101010100;
+       ReaDat = 32'b01010010110101010011001101111011;
+       MS2 = 1'b0;
+       #10;
+       MS2 = 1'b1;
+       #10;
+end
+          
 endmodule
